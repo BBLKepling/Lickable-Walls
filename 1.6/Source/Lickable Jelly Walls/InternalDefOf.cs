19679a7c37f082464e0a583775166c5210d102e5
BBLKepling <90485361+BBLKepling@users.noreply.github.com>
2025-06-16 15:17:30 -0700

    1.5 pdb, 1.6 update

---
+++ 1.6/Source/Lickable Jelly Walls/InternalDefOf.cs
@@ -0,0 +1,24 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Lickable_Walls
+{
+    [DefOf]
+    public static class InternalDefOf
+    {
+        static InternalDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(InternalDefOf));
+        }
+
+        public static BodyPartDef Tongue;
+        public static JobDef BBLK_Job_LickWall;
+        public static ThingDef BBLK_LickableWallpaper;
+
+        [MayRequire("Argon.ExpandedMaterials.Metals")]
+        public static HediffDef EM_LeadPoisoning;
+
+        [MayRequire("VE.VanillaChristmasExpanded")]
+        public static ThingDef VCE_Gingerbread;
+    }
+}
---
+++ 1.6/Source/Lickable Jelly Walls/JobDriver_LickWall.cs
@@ -0,0 +1,56 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using Verse.AI;
+
+namespace Lickable_Walls
+{
+    public class JobDriver_LickWall : JobDriver
+    {
+        public override bool TryMakePreToilReservations(bool errorOnFailed)
+        {
+            return true;
+        }
+
+        protected override IEnumerable<Toil> MakeNewToils()
+        {
+            this.FailOnDespawnedNullOrForbidden(TargetIndex.A);
+            yield return Toils_Goto.GotoCell(TargetIndex.A, PathEndMode.Touch);
+            Toil toil = ToilMaker.MakeToil("MakeNewToils");
+            if (ModLister.HasActiveModWithName("Expanded Materials - Metals") && base.TargetThingA.def == InternalDefOf.BBLK_LickableWallpaper)
+            {
+                toil.tickIntervalAction = delegate (int delta)
+                {
+                    pawn.rotationTracker.FaceTarget(base.TargetA);
+                    pawn.GainComfortFromCellIfPossible(delta);
+                    JoyUtility.JoyTickCheckEnd(pawn, delta, joySource: (Building)base.TargetThingA, fullJoyAction: JoyTickFullJoyAction.EndJob);
+                    if (ticksLeftThisToil % 60 != 0 || Rand.Bool) return;
+                    if (pawn.health.hediffSet.TryGetHediff(InternalDefOf.EM_LeadPoisoning, out Hediff hediff)) hediff.Severity += .01f;
+                    else pawn.health.AddHediff(InternalDefOf.EM_LeadPoisoning);
+                };
+            }
+            else
+            {
+                toil.tickIntervalAction = delegate (int delta)
+                {
+                    pawn.rotationTracker.FaceTarget(base.TargetA);
+                    pawn.GainComfortFromCellIfPossible(delta);
+                    JoyUtility.JoyTickCheckEnd(pawn, delta, joySource: (Building)base.TargetThingA, fullJoyAction: JoyTickFullJoyAction.EndJob);
+                };
+            }
+            toil.handlingFacing = true;
+            toil.defaultCompleteMode = ToilCompleteMode.Delay;
+            toil.defaultDuration = job.def.joyDuration;
+            yield return toil;
+        }
+        
+        public override object[] TaleParameters()
+        {
+            return new object[2]
+            {
+            pawn,
+            base.TargetA.Thing.def
+            };
+        }
+    }
+}
